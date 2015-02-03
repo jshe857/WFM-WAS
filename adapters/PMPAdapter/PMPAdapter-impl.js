3b0708557384f29b5e46318c6c6f1333ed2a396d
Jeffrey Shen <jeshen@au1.ibm.com>
2015-02-03 15:49:10 +1100

    perform csv parsing into json

--- adapters/PMPAdapter/PMPAdapter-impl.js
+++ adapters/PMPAdapter/PMPAdapter-impl.js
@@ -5,55 +5,114 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-/**
- *  WL.Server.invokeHttp(parameters) accepts the following json object as an argument:
- *  
- *  {
- *  	// Mandatory 
- *  	method : 'get' , 'post', 'delete' , 'put' or 'head' 
- *  	path: value,
- *  	
- *  	// Optional 
- *  	returnedContentType: any known mime-type or one of "json", "css", "csv", "javascript", "plain", "xml", "html"  
- *  	returnedContentEncoding : 'encoding', 
- *  	parameters: {name1: value1, ... }, 
- *  	headers: {name1: value1, ... }, 
- *  	cookies: {name1: value1, ... }, 
- *  	body: { 
- *  		contentType: 'text/xml; charset=utf-8' or similar value, 
- *  		content: stringValue 
- *  	}, 
- *  	transformation: { 
- *  		type: 'default', or 'xslFile', 
- *  		xslFile: fileName 
- *  	} 
- *  } 
- */
-var clientID = "5dceb7cd-b733-4281-8a95-79530a0d4a96"
-var groupName = "blue"
-var blueGroupUrl = "https://w3.api.ibm.com/common/run/bluegroup/members/"+ groupName +"?attrib=uid&client_id=" + clientID
-var csvServer = "sydgsa.ibm.com/projects/p/practitioneravailability/"
+
+var clientID = "5dceb7cd-b733-4281-8a95-79530a0d4a96";
+var groupName = "blue";
+var blueGroupUrl = "https://w3.api.ibm.com/common/run/bluegroup/members/"+ groupName +"?attrib=uid&client_id=" + clientID;
+var csvServer = "sydgsa.ibm.com/projects/p/practitioneravailability/";
 
 function getCSVList() {
-	
-	
-	WL.Logger.error("test");
-	WL.Logger.warn("test");
-	//WL.Logger.warn(WL.Server.configuration);
-	//WL.Logger.warn(WL.Server.configuration["ssl.keystore.path"]);
-	
 	var input = {
 	        method : 'get',
 	        returnedContentType : 'csv',
 	        path : "/projects/p/practitioneravailability/GBS Bench Report.csv"
-	    };
+	 };
 	
 	
 	//			sydgsa.ibm.com
 	//			/projects/p/practitioneravailability/GBS%20Bench%20Report.csv
 	//			sydgsa.ibm.com/projects/p/practitioneravailability/GBS%20Bench%20Report.csv
+	
+	
 
-	return WL.Server.invokeHttp(input);
+	var response = WL.Server.invokeHttp(input);	
+	var data = response.text;
+	
+	var array = CSVToArray(data);
+	return {"data":array};
+}
+
+function CSVToArray( strData, strDelimiter ){
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+        ),
+        "gi"
+        );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec( strData )){
+
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[ 1 ];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+            ){
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push( [] );
+
+        }
+
+        var strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]){
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[ 2 ].replace(
+                new RegExp( "\"\"", "g" ),
+                "\""
+                );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[ 3 ];
+
+        }
+
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push( strMatchedValue );
+    }
+
+    // Return the parsed data.
+    return( arrData );
 }
 
 
